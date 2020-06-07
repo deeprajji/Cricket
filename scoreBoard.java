3cff67b2f07e1cabc6ec990581b790bffd2c79b2
deeprajji <50217394+deeprajji@users.noreply.github.com>
2020-06-06 21:53:35 -0400

    Add files via upload

--- scoreBoard.java
+++ scoreBoard.java
@@ -1,5 +1,6 @@
 package Cricket;
 import java.awt.BorderLayout;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,14 +23,18 @@ public class scoreBoard implements ActionListener {
 	int scoreboardRuns;
 	NewBallGUI newBallacted;
 	
-	private double overs= 0;
+	private double overs= 0;//firstInning overs
+	private double secondInningOvers = 0; //second inning overs
 	private int balls = 0;
+	private int secondInningBalls = 0; // second inning balls
 	private int team1runs= 0;
-	private String result= "";
+	private int team2runs = 0;
+	
 	private int outs= 0;
-	private double runRate = 0.0;
-	private int projected = 0;
+	private int secondInningOUts = 0;
+	
 	private int ScoreToBeat;
+	private int lastBall;
 	ArrayList<Player> playersOnStrike = new ArrayList<>();
 	private double maxOvers;
 	Team realteam1;
@@ -38,12 +43,15 @@ public class scoreBoard implements ActionListener {
 	Team BowlingTeam;
 	ArrayList<Player> playerBowling = new ArrayList<>();
 	JLabel label;
+	private double requiredStrikeRate;
+	private int ballsLeft;
 public scoreBoard(double maxOvers, Team team1, Team team2){
 	this.maxOvers = maxOvers;
 	this.realteam1 = team1;
 	this.realteam2 = team2;
-	 label = new JLabel("Score: " + team1runs);
-	JButton newBall = new JButton("New Ball");
+	
+	 
+/*	JButton newBall = new JButton("New Ball");
 	
 	
 	newBall.addActionListener(new ActionListener() {
@@ -74,83 +82,153 @@ public void actionPerformed(ActionEvent e) {
 }
 public int returnResultOfBall() {
 	return resultOfBall;
+	*/
 }
-public int firstInnings() {
-	battingTeam = realteam1;//will add coin toss
-	BowlingTeam = realteam2;
-	
-	Player temp = new Player("temp");
-	playersOnStrike.add(battingTeam.team.get(0));
-	playersOnStrike.add(battingTeam.team.get(1));
-	playerBowling.add(BowlingTeam.team.get(0));
-	int counter = 1; 
-	while(overs <= maxOvers && outs < battingTeam.team.size()) {
-		if(overs%6 ==0.0) {
-			//need to change bowler
-			temp = playersOnStrike.get(0);
-			playersOnStrike.set(0,playersOnStrike.get(1));
-			playersOnStrike.set(1, temp);
-		}
-		
-		balls++;
-		this.recalculateOvers();
-		if(resultOfBall > -1) {
-			team1runs += resultOfBall;
-			playersOnStrike.get(0).addRuns(this.returnResultOfBall());
-			playersOnStrike.get(0).addBallsFaced();
-		//need to add Team Runs
-			playerBowling.get(0).addRunsAllowed(this.returnResultOfBall());
-			playerBowling.get(0).addBallsDealt();
-			if(this.returnResultOfBall() % 2 == 0) {
-				
-			}
-			else {
-				temp = playersOnStrike.get(0);
-				playersOnStrike.set(0, playersOnStrike.get(1));
-				playersOnStrike.set(1, temp);
-			}
-		}
-		else {
-			outs++;
-			playerBowling.get(0).addBallsDealt();
-			playerBowling.get(0).addWickets(1);
-			playersOnStrike.get(0).addBallsFaced();
-			//add team outs
-			if(counter< battingTeam.team.size()) {
-				
-			
-			counter ++;
-			playersOnStrike.set(0, battingTeam.team.get(counter));
-			}
-		}
-	frame.repaint();	
-	}
-	
-	
-ScoreToBeat = team1runs;
-	
-	
-	
-	
-	
-	
-	
-	
-	return team1runs;
-}
+
 public void recalculateOvers(){
 	overs = (double)balls/6;
 }
-public int returnRuns() {
-	return team1runs;
+public void recalculateSecondInningOvers() {
+	secondInningOvers = (double)balls/6;
 }
-public static void main(String[] args) {
-	GameSetUp x = new GameSetUp();
+public static void main(String[] args) throws InterruptedException {
+	settingUpGame x = new settingUpGame();
 	
 	setUpOvers y = new setUpOvers();
 	scoreBoard firstTrial = new scoreBoard(y.returnMaxOvers(), x.team1, x.team2);
-	System.out.print("Came back");
-	firstTrial.firstInnings();
-			
+	firstTrial.battingTeam = firstTrial.realteam1;
+	firstTrial.BowlingTeam = firstTrial.realteam2;
+	JFrame ballResultAsker;
+	while(firstTrial.overs < firstTrial.maxOvers && firstTrial.outs < firstTrial.battingTeam.team.size()) {
+		ballResultAsker = new JFrame();
+		String result = JOptionPane.showInputDialog(ballResultAsker,"what was the result of the ball");
+		firstTrial.resultOfBall = Integer.parseInt(result);
+		if(firstTrial.resultOfBall > -1 && firstTrial.resultOfBall < 7) {
+			firstTrial.team1runs += firstTrial.resultOfBall;
+			firstTrial.balls ++;
+			firstTrial.recalculateOvers();
+		}
+		else {
+			firstTrial.outs ++;
+			firstTrial.balls++;
+			firstTrial.recalculateOvers();
+		}
+		
+		JLabel score = new JLabel("Score: " + firstTrial.team1runs + "-" + firstTrial.outs);
+		JLabel numberOfOvers = new JLabel("Overs: " + firstTrial.overs);
+		JFrame scoreBoard = new JFrame();
+		JPanel ScoreBoardPanel = new JPanel();
+		ScoreBoardPanel.setBorder(BorderFactory.createEmptyBorder(90,90,90,90));
+		ScoreBoardPanel.setLayout(new GridLayout(0, 1));
+		
+		scoreBoard.add(ScoreBoardPanel, BorderLayout.CENTER);
+		scoreBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		scoreBoard.setTitle("Our GUI");
+		scoreBoard.pack(); 
+		scoreBoard.setVisible(true);
+		
+		ScoreBoardPanel.add(score);
+		ScoreBoardPanel.add(numberOfOvers);
+		Thread.sleep(5000); // waits 5 seconds before it removes the frame and goes to the next ball
+		scoreBoard.dispose();
+		
+	}
+	firstTrial.ScoreToBeat = firstTrial.team1runs;
+	firstTrial.BowlingTeam = firstTrial.realteam1;
+	firstTrial.battingTeam = firstTrial.realteam2;
+	while((firstTrial.secondInningOvers <= firstTrial.maxOvers) && (firstTrial.secondInningOUts<firstTrial.battingTeam.team.size()) && (firstTrial.team2runs <= firstTrial.ScoreToBeat)) {
+		ballResultAsker = new JFrame();
+		String result = JOptionPane.showInputDialog(ballResultAsker, "What was the result of the ball?");
+		firstTrial.resultOfBall = Integer.parseInt(result);
+		if(firstTrial.resultOfBall > -1 && firstTrial.resultOfBall < 7) {
+			firstTrial.team2runs += firstTrial.resultOfBall;
+			firstTrial.secondInningBalls++;
+			firstTrial.recalculateSecondInningOvers();
+		}
+		else {
+			firstTrial.secondInningOUts++;
+			firstTrial.secondInningBalls++;
+			firstTrial.recalculateSecondInningOvers();
+		}
+		firstTrial.ballsLeft = (int)(firstTrial.maxOvers * 6) - firstTrial.secondInningBalls;
+		firstTrial.requiredStrikeRate = (double)(firstTrial.ScoreToBeat - firstTrial.team2runs)/ firstTrial.ballsLeft;
+		JLabel score = new JLabel("Score: " + firstTrial.team2runs +" - " + firstTrial.secondInningOUts);
+		JLabel numberOfOvers = new JLabel("Overs: " + firstTrial.secondInningOvers);
+		JLabel numberOfBallsLeft = new JLabel("Number of Balls Left: " + firstTrial.ballsLeft);
+		JLabel requiredStrikeRate = new JLabel("Required Strike Rate to win: " + firstTrial.requiredStrikeRate);
+		JLabel whichInning = new JLabel("We are in Inning 2!");
+		
+		JFrame scoreBoard = new JFrame();
+		JPanel ScoreBoardPanel = new JPanel();
+		ScoreBoardPanel.setBorder(BorderFactory.createEmptyBorder(90,90,90,90));
+		ScoreBoardPanel.setLayout(new GridLayout(0, 1));
+		
+		scoreBoard.add(ScoreBoardPanel, BorderLayout.CENTER);
+		scoreBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		scoreBoard.setTitle("Second Inning");
+		scoreBoard.pack(); 
+		scoreBoard.setVisible(true);
+		
+		ScoreBoardPanel.add(whichInning);
+		ScoreBoardPanel.add(score);
+		ScoreBoardPanel.add(numberOfOvers);
+		ScoreBoardPanel.add(numberOfBallsLeft);
+		ScoreBoardPanel.add(requiredStrikeRate);
+		Thread.sleep(5000); // waits 5 seconds before it removes the frame and goes to the next ball
+		scoreBoard.dispose();
+		
+	}
+	
+	if(firstTrial.team2runs > firstTrial.ScoreToBeat) {
+		JFrame winner = new JFrame();
+		JPanel winnerPanel = new JPanel();
+		winnerPanel.setBorder(BorderFactory.createEmptyBorder(90,90,90,90));
+		winnerPanel.setLayout(new GridLayout(0, 1));
+		
+		winner.add(winnerPanel, BorderLayout.CENTER);
+		winner.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		winner.setTitle("Second Inning");
+		winner.pack(); 
+		winner.setVisible(true);
+		int wicketsleft = firstTrial.realteam2.team.size()-firstTrial.secondInningOUts;
+		JLabel winningTeam = new JLabel("Congratulations to the team who batted second! You have won with "+ wicketsleft + " Wickets Left!!");
+		winnerPanel.add(winningTeam);
+		
+	}
+	else if(firstTrial.team2runs < firstTrial.ScoreToBeat) {
+		JFrame winner = new JFrame();
+		JPanel winnerPanel = new JPanel();
+		winnerPanel.setBorder(BorderFactory.createEmptyBorder(90,90,90,90));
+		winnerPanel.setLayout(new GridLayout(0, 1));
+		
+		winner.add(winnerPanel, BorderLayout.CENTER);
+		winner.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		winner.setTitle("result");
+		winner.pack(); 
+		winner.setVisible(true);
+		int runsLeft = firstTrial.ScoreToBeat - firstTrial.team2runs;
+		JLabel winningTeam = new JLabel("Congratulations to the team that batted first! you have won by a margin of " + runsLeft +" runs!!");
+		winnerPanel.add(winningTeam);
+	}
+	else {
+		JFrame winner = new JFrame();
+		JPanel winnerPanel = new JPanel();
+		winnerPanel.setBorder(BorderFactory.createEmptyBorder(90,90,90,90));
+		winnerPanel.setLayout(new GridLayout(0, 1));
+		
+		winner.add(winnerPanel, BorderLayout.CENTER);
+		winner.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		winner.setTitle("result");
+		winner.pack(); 
+		winner.setVisible(true);
+		JLabel draw = new JLabel("Game ends in a draw, create a new game to decide the winner");
+		winnerPanel.add(draw);
+	}
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 }
